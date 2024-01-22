cd01f2d8121ed9cabc231d6353db27cd6d4fae47
inne <mrbelongtim@gmail.com>
2024-01-22 19:40:47 +0100

    added new links

--- my-app/src/components/header/AppHeader.js
+++ my-app/src/components/header/AppHeader.js
@@ -5,28 +5,43 @@ import './AppHeader.css'; // Создайте файл AppHeader.css для ст
 const AppHeader = () => {
   return (
     <header className="app-header">
-      <div className="wrapper">
-      <div className="logo">
-        <img src={require('../../images/logo.png')} alt="Logo" />
+     <div className="wrapper">
+          <div className="logo">
+            <img src={require('../../images/logo.png')} alt="Logo" />
+          </div>
+
+		<div className="header_links">
+			<nav className="nav-links">
+				<a href="/">Home</a>
+				<a href="/about">Feautures</a>
+				<a href="/services">News</a>
+				<a href="/contact">Contact</a>
+			</nav>
+
+			<div className="search-bar">
+				<input type="text" placeholder="Search" />
+			</div>
+
+			<div className="social-links">
+				<a href="#" target="_blank" rel="noopener noreferrer"><FaFacebook size={30}/></a>
+				<a href="#" target="_blank" rel="noopener noreferrer"><FaTwitter size={30}/></a>
+				<a href="#" target="_blank" rel="noopener noreferrer"><FaInstagram size={30}/></a>
+			</div>
+		</div>
       </div>
 
-      <nav className="nav-links">
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/services">Services</a>
-        <a href="/contact">Contact</a>
-      </nav>
-
-      <div className="search-bar">
-        <input type="text" placeholder="Search" />
-      </div>
-
-      <div className="social-links">
-        <a href="#" target="_blank" rel="noopener noreferrer"><FaFacebook size={30}/></a>
-        <a href="#" target="_blank" rel="noopener noreferrer"><FaTwitter size={30}/></a>
-        <a href="#" target="_blank" rel="noopener noreferrer"><FaInstagram size={30}/></a>
-      </div>
-      </div>
+	  <div className="header_bottom">
+		<nav className="bottom-links">
+			<a href="#">Table of Matches</a>
+			<a href="#">Updates</a>
+			<a href="#">Calendars</a>
+			<a href="#">Top Scores</a>
+			<a href="#">League Table</a>
+			<a href="#">Team Table</a>
+			<a href="#">Events</a>
+			<a href="#">Sponsors</a>
+		</nav>
+	  </div>
     </header>
   );
 };
